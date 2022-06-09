4c08e81078efbb4e39c8af6fe734032b1fde9cfb
urtimka <urtimka@gmail.com>
2022-06-09 10:26:24 +0400

    Test commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.print("glhf");
+        hello("world");
+
+        double li = 5;
+        System.out.println("Площадь квадрата "+li+"="+area(li));
+    }
+    public static void hello(String x){
+        System.out.println("Hello " + x+"!!");
+    }
+
+    public static double area(double l){
+        return l*l;
     }
 }
