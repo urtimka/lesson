c50850cdfaf0a144348a0ae8656a983b3a9c6411
urtimka <urtimka@gmail.com>
2022-06-09 11:06:21 +0400

    Test commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
+import org.w3c.dom.css.Rect;
+
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        hello("world");
 
-        double li = 5;
-        System.out.println("Площадь квадрата "+li+"="+area(li));
+
+        Rectangle r = new Rectangle(4,6);//треугольник
+        System.out.println("Площадь треугольника " + r.a + " и " + r.b + " = " + area(r));
+
+
+        Square s = new Square(5); //квадрат
+        System.out.println("Площадь квадрата " + s.l + " = " + area(s));
     }
+
     public static void hello(String x){
-        System.out.println("Hello " + x+"!!");
+        System.out.println("Hello " + x +"!!");
     }
 
-    public static double area(double l){
-        return l*l;
+    public static double area(Square s){
+        return s.l * s.l;
+    }
+    public static double area(Rectangle r){
+        return r.a * r.b;
     }
 }
---
+++ src/Square.java
@@ -0,0 +1,7 @@
+public class Square {
+    public double l;
+
+    public Square(double l){
+        this.l = l;
+    }
+}
