e5b55beec959add35022ef1696984bdc380865d5
urtimka <urtimka@gmail.com>
2022-06-09 16:23:43 +0400

    Test commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle(4, 6);//треугольник
+        System.out.println("Площадь треугольника " + r.a + " и " + r.b + " = " + r.area());
+
+        Square s = new Square(5); //квадрат
+        System.out.println("Площадь квадрата " + s.l + " = " + s.area());
+
+
+        Point a = new Point(1, 6);
+        Point b = new Point(5, 9);
+        System.out.println("Расстояние между двумя точками равно (вычисление функцией) " + Point.distance(a, b));
+        System.out.println("Расстояние между двумя точками равно (вычисление методом) " + a.distance(b));
+    }
+
+}
+
--- src/main/java/Square.java
+++ src/main/java/Square.java
@@ -1,7 +1,9 @@
 public class Square {
     public double l;
-
     public Square(double l){
         this.l = l;
+    }
+    public double area(){
+        return this.l * this.l;
     }
 }
